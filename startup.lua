d5019a53256fecdd3ec4a3174198bdc06b143a66
Diego García <dagarciam1014@gmail.com>
2025-04-23 12:30:58 -0500

    updated

---
+++ startup.lua
@@ -0,0 +1,16 @@
+local modules = {}
+local repoUser = "DiegoG1019"
+local repoName = "CreateAstral.StarStream"
+local ghDownloadAddr = "https://raw.githubusercontent.com/"..repoUser.."/"..repoName.."/refs/heads/master/"
+local ghQueryAddr = "https://api.github.com/repos/"..repoUser.."/"..repoName.."/git/trees"
+
+function update()
+{
+	
+}
+
+-- update
+
+-- make every computer resetable at any time, also for updates
+
+-- event handling
